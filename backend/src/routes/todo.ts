17905a18506743cb9ce9419f803c0c6f0793ddb4
gino0950150 <gino0950150@gmail.com>
2021-07-09 08:43:17 +0000

    done

--- backend/src/routes/todo.ts
+++ backend/src/routes/todo.ts
@@ -9,10 +9,54 @@ const TodoRouter = (server: FastifyInstance, opts: RouteShorthandOptions, done: 
     interface IdParam {
         id: string
     }
-
-    // TODO: Add CRUD endpoints, i.e. get, post, update, delete
-    // NOTE: the url should be RESTful
-
+    server.get('/todos', opts, async (request, reply) => {
+        try{
+            const todos: Array<ITodo> = await todoRepo.getTodos()
+            return reply.status(200).send({todos})
+        } catch(error) {
+            console.error(`GET /todos Err: ${error}`)
+            return reply.status(500).send(`[Server Err]: ${error}`)
+        }   
+    })
+    server.post('/todos', opts, async (request, reply) => {
+        try{
+            const todoBody: ITodo = request.body as ITodo
+            const todo: ITodo = await todoRepo.addTodo(todoBody)
+            return reply.status(201).send({todo})
+        } catch(error) {
+            console.error(`Post /todo Err: ${error}`)
+            return reply.status(500).send(`[Server Err]: ${error}`)
+        }   
+    })
+    server.put<{ Params: IdParam }>('/todos/:id', opts, async (request, reply) => {
+        try {
+            const id = request.params.id
+            const todoBody = request.body as ITodo
+            const todo: ITodo | null = await todoRepo.updateTodo(id, todoBody)
+            if (todo) {
+                return reply.status(200).send({ todo })
+            } else {
+                return reply.status(404).send({ msg: `Not Found Todo:${id}` })
+            }
+        } catch (error) {
+            console.error(`PUT /todos/${request.params.id} Error: ${error}`)
+            return reply.status(500).send(`[Server Error]: ${error}`)
+        }
+    })
+    server.delete<{ Params: IdParam }>('/todos/:id', opts, async (request, reply) => {
+        try {
+            const id = request.params.id
+            const todo: ITodo | null = await todoRepo.deleteTodo(id)
+            if (todo) {
+                return reply.status(204).send()
+            } else {
+                return reply.status(404).send({ msg: `Not Found Todo:${id}` })
+            }
+        } catch (error) {
+            console.error(`DELETE /todos/${request.params.id} Error: ${error}`)
+            return reply.status(500).send(`[Server Error]: ${error}`)
+        }
+    })
     done()
 }
 
--- frontend/src/API.ts
+++ frontend/src/API.ts
@@ -1,17 +1,51 @@
 import axios, { AxiosResponse } from 'axios'
 
 const getTodos = async (): Promise<AxiosResponse<Array<ITodo>>> => {
-// TODO: Should call GET endpoint
+    try{
+        const todos = await axios.get('/api/todos')
+        return todos
+    } catch(err) {
+        console.error(`GET /api/todos ERR: ${err}`)
+        throw new Error(err)
+    }
 }
 
-// TODO: Should call POST endpoint
+const addTodo = async (todoBody: ITodo): Promise<AxiosResponse<ITodo>> => {
+    try{
+        const newTodo = {
+            ...todoBody,
+            status: false
+        }
+        const todo = await axios.post('/api/todos', newTodo)
+        return todo
+    } catch(err) {
+        console.error(`POST /api/todos ERR: ${err}`)
+        throw new Error(err)
+    }
+}
 
 const updateTodo = async (todoBody: ITodo): Promise<AxiosResponse<ITodo>> => {
-// TODO: Should call PUT endpoint
+    try{
+        const newTodo = {
+            ...todoBody,
+            status: true
+        }
+        const todo = await axios.put(`/api/todos/${todoBody._id}`, newTodo)
+        return todo
+    } catch(err) {
+        console.error(`PUT /api/todos/${todoBody._id} ERR: ${err}`)
+        throw new Error(err)
+    }
 }
 
 const deleteTodo = async (id: string): Promise<AxiosResponse> => {
-// TODO: Should call DELETE endpoint
+    try{
+        const todo = await axios.delete(`/api/todos/${id}`)
+        return todo
+    } catch(err) {
+        console.error(`DELETE /api/todos/${id} ERR: ${err}`)
+        throw new Error(err)
+    }
 }
 
 export { getTodos, addTodo, updateTodo, deleteTodo }
